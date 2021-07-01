013e28a834ad2f135465f8c82066be1f80c95fbb
Armando <armando.espinozza@gmail.com>
2021-06-30 20:09:14 -0500

    sCorregido logo  y error de json en registro

--- js/registro.js
+++ js/registro.js
@@ -1,24 +1,25 @@
-document.getElementById("crear").addEventListener("click", crear);
+//document.getElementById("crear").addEventListener('click', crear);
 
 function crear(){
     let email =   document.getElementById("email").value;
     let user =   document.getElementById("user").value;  
     let password =   document.getElementById("password").value;  
     let confirmPassword =   document.getElementById("confirmPassword").value;
-    if(password != confirmPassword){
-     alert('tus constraseñas no coinciden') 
-     return false;
-    }
+  
    var newRegis ={
        email:email,
        user:user,
        password:password,
    };
-   var regisToJson =JSON.stringify(newRegis);
-   console.log(regisToJson)
-    /*localStorage.setItem("email",email);
-    localStorage.setItem("user",user);
-    localStorage.setItem("password",btoa(password));*/
+
+   if(password != confirmPassword){
+    alert('tus constraseñas no coinciden') 
+    return false;
+   } else {
+    var regisToJson =JSON.stringify(newRegis);
+    console.log(regisToJson)
+   }
+  
 }
 
 function cancelar(){
@@ -29,6 +30,13 @@ function cancelar(){
     
 
 }
+
+
+  /*localStorage.setItem("email",email);
+    localStorage.setItem("user",user);
+    localStorage.setItem("password",btoa(password));*/
+
+
 /*var newProduct = {
     productName: productName,
     productDescription: productDescription,
