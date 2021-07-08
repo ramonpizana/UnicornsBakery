99a29a65ff1133f5ca1ca1318e437671919ab368
chrisemm96 <84466794+chrisemm96@users.noreply.github.com>
2021-07-08 12:34:09 -0500

    product localstorage

--- js/product.js
+++ js/product.js
@@ -1,17 +1,21 @@
-function addItem(item){
+//const { json } = require("body-parser");
+
+window.onload=addItem();
+function addItem(){
+  var items = JSON.parse(localStorage.getItem("newProduct"));
+var keysProduct = Object.keys(items);
+  for(let i = 0; i < keysProduct.length;i++){
   const itemHTML =   '<div class="producto">\n'+
- ' <img class="imgProducto" src="/rsrcs/productos/'+item.productImage+'" alt="">\n'+
- ' <h5>'+item.productName+'</h5>\n'+
- ' <p>'+item.productDescription+'</p>\n'+
+ ' <img class="imgProducto" src="/rsrcs/productos/'+items[i].productImage+'" alt="">\n'+
+ ' <h5>'+items[i].productName+'</h5>\n'+
+ ' <p>'+items[i].productDescription+'</p>\n'+
  '   </div>\n';
   const itemsContainer = document.getElementById("newProducts");
   itemsContainer.innerHTML += itemHTML;
+}
 } 
-
-
-
-
 function submitProduct() {
+  verifyProducts();
   var allowedExtension = ['jpeg', 'jpg', 'png'];
   var fileExtension = document.getElementById('validateProductImage').value.split('.').pop().toLowerCase();
   var isValidFile = false;
@@ -24,16 +28,34 @@ function submitProduct() {
       var productDescription = document.getElementById("productDescription").value;
       var productImage = document.getElementById("validateProductImage").value;
 
-      var newProduct = {
-        productName: productName,
-        productDescription: productDescription,
-        productImage: productImage
-      };
-      var prodcutJson = JSON.stringify(newProduct);
+      /*var prodcutJson = JSON.stringify(newProduct);
       addItem({'productName':productName,
   'productImage':productImage,
   'productDescription':productDescription});
-      console.log(prodcutJson);
+  
+      console.log(prodcutJson);*/
+    var newProducts = {
+      productName:productName,
+      productDescription:productDescription,
+      productImage:productImage
+    };
+    
+var traerProduct = JSON.parse(localStorage.getItem('newProduct'));
+ var keysProduct = Object.keys(traerProduct);
+ 
+ for(let i = 0; i < keysProduct.length;i++){
+     if( newProducts.productName == traerProduct[i].productName){
+        M.toast({html: 'Este producto ya existe',classes: 'rounded'});
+        return
+    }
+ }
+
+     traerProduct[keysProduct.length]=newProducts;
+
+//  console.log(traer);
+var productToJson = JSON.stringify(traerProduct);
+localStorage.setItem('newProduct',productToJson);
+
 
 
       break;
@@ -47,9 +69,14 @@ function submitProduct() {
   return isValidFile;
 
 }
+function verifyProducts(){
+  if(!localStorage.getItem('newProduct')){
+    localStorage.setItem('newProduct',JSON.stringify({}));
+  }
+}
 
 
-
+//0:{productName:'Pay',productDescription:'un pay',productImage:'/rsrcs/productos/pay.jpg'}
 
 
 
